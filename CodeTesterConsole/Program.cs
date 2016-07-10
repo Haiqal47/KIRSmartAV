47cb158a6ba6d0d94f6e1cfea005037276fe2592
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2016-07-10 20:01:35 +0700

    Added console project to test fragile codes.

---
+++ CodeTesterConsole/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using KIRSmartAV.Core;
+
+namespace CodeTesterConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encr = new FileEncoder();
+            encr.ProgressChanged += Encr_ProgressChanged;
+
+            string inputfile = "D:\\bin\\paa.vor";
+            string outputfile = "D:\\bin\\asasas.exe";
+
+            encr.DecryptFile(inputfile, outputfile);
+            Console.WriteLine("Encrypt 1 completed.");
+
+           // encr.EncryptFileAsync(inputfile, outputfile);
+
+            Console.WriteLine("Encrypt 2 completed.");
+            Console.ReadKey();
+        }
+
+        private static void Encr_ProgressChanged(object sender, FileEncoder.EncodeProgressChanged e)
+        {
+            Console.WriteLine(e.ProgressPercentage);
+        }
+    }
+}
