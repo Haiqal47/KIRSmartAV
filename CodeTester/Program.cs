a26270e324634ac5b06d9919685e4910048b0f68
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2016-07-10 20:20:54 +0700

    Included console project for testing fragile code.

---
+++ CodeTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
