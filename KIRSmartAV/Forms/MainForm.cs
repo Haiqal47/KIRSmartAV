d8218354882d9ba2015adc260a8a1b8cf1baa732
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2016-07-11 08:42:49 +0700

    Revised license headers

--- KIRSmartAV/Forms/MainForm.cs
+++ KIRSmartAV/Forms/MainForm.cs
@@ -1,16 +1,22 @@
 ﻿/*   
-  MainForm.cs (KIRSmartAV)
-  ====================================
-  This file is a part of Fahmi's work which it's copyright(s)
-  belongs to Fahmi Noor Fiqri as the owner of this code. Any 
-  reproduction, distribution, manipulation, or other actions that 
-  is not explicitly permitted by author is prohibited.
+      MainForm.cs (KIRSmartAV)
+      ============================================
+      Copyright(C) 2016  Fahmi Noor Fiqri
   
-  { Feel free to ask to the author to get rights to edit this file
-    visit this project at GitHub! https://github.com/fahminlb33 }
+      This program is free software: you can redistribute it and/or modify
+      it under the terms of the GNU Lesser General Public License as published by
+      the Free Software Foundation, either version 3 of the License, or
+      (at your option) any later version.
   
-  Copyright (C) Fahmi Noor Fiqri 2016. All Rights Reserved.
+      This program is distributed in the hope that it will be useful,
+      but WITHOUT ANY WARRANTY; without even the implied warranty of   
+      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+      GNU Lesser General Public License for more details.
+  
+      You should have received a copy of the GNU Lesser General Public License
+      along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
+
 using System;
 using System.Windows.Forms;
 using KIRSmartAV.ToolsForms;
