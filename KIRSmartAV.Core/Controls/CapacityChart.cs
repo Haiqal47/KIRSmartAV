41b205a958d7b05e1d2edecab6b6e39831cf226a
Fahmi Noor Fiqri <fahminlb33@gmail.com>
2016-07-12 09:21:19 +0700

    Invalidation fixed

--- KIRSmartAV.Core/Controls/CapacityChart.cs
+++ KIRSmartAV.Core/Controls/CapacityChart.cs
@@ -28,30 +28,24 @@ namespace KIRSmartAV.Core.Controls
     public class CapacityChart : Control
     {
         private float _chartPie = 0F;
-        private bool _shouldInvalidate = false;
 
         public void DrawPie(long totalSpace, long freeSpace)
         {
             _chartPie = 360.0F * freeSpace / totalSpace;
             this.Invalidate();
-            _shouldInvalidate = true;
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
-            if (_shouldInvalidate)
-            {
-                var G = e.Graphics;
+            var G = e.Graphics;
+            G.Clear(BackColor);
 
-                var Frame = new Rectangle(1, 1, this.Width - 1, this.Height - 1);
-                G.DrawRectangle(new Pen(BackColor), Frame);
+            var Frame = new Rectangle(1, 1, this.Width - 1, this.Height - 1);
+            G.DrawRectangle(new Pen(BackColor), Frame);
 
-                var GraphicChart = new Rectangle(5, 5, this.Width - 10, this.Height - 10);
-                G.FillPie(Brushes.Blue, GraphicChart, _chartPie, 360 - _chartPie);
-                G.FillPie(Brushes.Magenta, GraphicChart, 0, _chartPie);
-
-                _shouldInvalidate = false;
-            }
+            var GraphicChart = new Rectangle(5, 5, this.Width - 10, this.Height - 10);
+            G.FillPie(Brushes.Blue, GraphicChart, _chartPie, 360 - _chartPie);
+            G.FillPie(Brushes.Magenta, GraphicChart, 0, _chartPie);
 
             base.OnPaint(e);
         }
